2de1a14904b297a9b93e6985218a1392a49a76d1
Gingershine <142210638+Gingershine@users.noreply.github.com>
2024-01-19 18:31:18 +0200

    fixes

--- js/task-3.js
+++ js/task-3.js
@@ -3,6 +3,6 @@ const userNameOutput = document.querySelector('#name-output');
 
 userNameInput.addEventListener('input', event => {
     userNameOutput.textContent = event.currentTarget.value.trim()
-    ? event.currentTarget.value
+    ? event.currentTarget.value.trim()
     : 'Anonymous';
 });
